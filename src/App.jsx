7d7d9cdd5fed438f4f2a07c76d50bcad5055512d
ClarissaMc <clarissaissy05@gmail.com>
2024-05-20 18:39:59 -0400

    refactored removing story to be inline

--- src/App.jsx
+++ src/App.jsx
@@ -104,10 +104,6 @@ const List = ({ list, onRemoveItem }) => {
 }
 
 const Item = ({ item, onRemoveItem }) => {
-  const handleRemoveItem = () => {
-    onRemoveItem(item);
-  };
-
   return (
     <li>
       <span>
@@ -117,7 +113,7 @@ const Item = ({ item, onRemoveItem }) => {
       <span>{item.num_comments}</span>
       <span>{item.points}</span>
       <span>
-        <button type="button" onClick={handleRemoveItem}>
+        <button type="button" onClick={() => onRemoveItem(item)}>
           Dismiss
         </button>
       </span>
