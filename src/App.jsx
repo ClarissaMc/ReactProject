2951b8ac89225e75908e263dbf5f8e640821bcfa
ClarissaMc <clarissaissy05@gmail.com>
2024-05-25 14:57:11 -0400

    added submit button for searches

--- src/App.jsx
+++ src/App.jsx
@@ -56,17 +56,19 @@ const App = () => {
   
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
+
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false }  // this object is the state, which is named stories
   );
 
   const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return;
-
     dispatchStories({ type: STORIES_FETCH_INIT });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -77,7 +79,7 @@ const App = () => {
       .catch(() => {
         dispatchStories({ type: STORIES_FETCH_FAILURE })
       });
-  }, [searchTerm]);
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
@@ -91,9 +93,13 @@ const App = () => {
   };
 
   // Callback function introduced as event handler
-  const handleSearch = (event) => {
+  const handleSearchInput = (event) => {
     // Callback function calls back to the place it was introduced
     setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
   return (
@@ -105,10 +111,18 @@ const App = () => {
         id="search"
         value={searchTerm}
         isFocused
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
       >
         <strong>Search:</strong>
       </InputWithLabel>
+
+      <button
+        type="button"
+        disabled={!searchTerm}
+        onClick={handleSearchSubmit}
+      >
+        Submit
+      </button>
 
       <hr></hr>
 
