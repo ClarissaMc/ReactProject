86c2ad0578a87cc4f8b8cc17806a843d5187a37c
ClarissaMc <clarissaissy05@gmail.com>
2024-05-26 12:53:35 -0400

    replaced styling with CSS modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import axios from 'axios';
 
-import './App.css';
+import styles from './App.module.css';
+import clsx from 'clsx';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -106,14 +107,16 @@ const App = () => {
   };
 
   return (
-    <div className="container">
-      <h1 className="headline-primary">My Hacker Stories</h1>
+    <div className={styles.container}>
+      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
+      
+      <hr></hr>
 
       {stories.isError && <p>Something went wrong...</p>}
 
@@ -130,7 +133,7 @@ const App = () => {
 };
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit, }) => (
-  <form onSubmit={onSearchSubmit} className='search-form'>
+  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -140,7 +143,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit, }) => (
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button type="submit" disabled={!searchTerm} className='button button_large'>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className={clsx(styles.button, styles.buttonLarge)}
+    >
       Submit
     </button>
   </form>
@@ -148,7 +155,7 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit, }) => (
 
 const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children, }) => (
   <>
-    <label htmlFor={id} className='label'>{children}</label>
+    <label htmlFor={id} className={styles.label}>{children}</label>
     &nbsp;
     <input
       id={id}
@@ -156,7 +163,7 @@ const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, ch
       value={value}
       autoFocus={isFocused}
       onChange={onInputChange}
-      className='input'
+      className={styles.input}
     />
   </>
 )
@@ -175,7 +182,7 @@ const List = ({ list, onRemoveItem }) => {
 }
 
 const Item = ({ item, onRemoveItem }) => (
-  <li className='item'>
+  <li className={styles.item}>
     <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
     </span>
@@ -183,10 +190,10 @@ const Item = ({ item, onRemoveItem }) => (
     <span style={{ width: '10%' }}>{item.num_comments}</span>
     <span style={{ width: '10%' }}>{item.points}</span>
     <span style={{ width: '10%' }}>
-      <button 
+      <button
         type="button"
         onClick={() => onRemoveItem(item)}
-        className="button button_small"
+        className={clsx(styles.button, styles.buttonSmall)}
       >
         Dismiss
       </button>
